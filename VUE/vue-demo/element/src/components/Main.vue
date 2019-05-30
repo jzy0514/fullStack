84f9f9aeed1600032d03ae686617de0821ad7d88
JZYQAQ <1536729204@qq.com>
2019-05-30 21:25:29 +0800

    up

---
+++ VUE/vue-demo/element/src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-content">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from './common/Header' // 引用组件(把一个vue界面当做一个组件)
+export default {
+  name: 'mainContent',
+  data () {
+    return {
+      msg: 'hello world'
+    }
+  },
+  components: { // 申明注册
+    Header
+  }
+}
+</script>
--- VUE/vue-demo/element/src/components/login.vue
+++ VUE/vue-demo/element/src/components/login.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="login">
     <div class="container">
-      <p class="title">
-        后台管理系统
-      </p>
-      <div class="msg">
-        <el-input v-model="user" placeholder="请输入内容"></el-input>
+      <p class="title">后台管理系统</p>
+      <div class="msg mt20">
+        <el-input v-model="user" placeholder="请输入用户名"></el-input>
       </div>
       <div class="msg mt20">
-        <el-input placeholder="请输入密码" v-model="password" show-password></el-input>
+        <el-input v-model="password" placeholder="请输入密码"></el-input>
       </div>
       <div class="btn mt20">
-          <el-button type="primary" @click="login">登录</el-button>
+        <el-button type="primary" @click="login">登录</el-button>
       </div>
     </div>
   </div>
@@ -27,12 +25,17 @@ export default {
     }
   },
   methods: {
-   login () {
-     if (this.user && this.password) {
-       localStorage.setItem('userName', this.user);
-       localStorage.setItem('userPassword', this.password);
-     }
-   } 
+    login () {
+      if (this.user && this.password) {
+        localStorage.setItem('userName', this.user) // 存储账号
+        // 字符串
+        // this.$router.push('/main')
+        // 对象
+        // this.$router.push({ path: '/main' })
+        // 编程式跳转
+        this.$router.push({name: 'Main', params: {usrId: 1}})
+      }
+    }
   }
 }
 </script>
@@ -47,17 +50,17 @@ export default {
   margin: 0 auto;
   box-shadow: 0 0 20px #cac6c6;
   padding: 50px;
-  margin-top: 150px;
+  margin-top: 200px;
 }
 .title{
   font-size: 26px;
   font-weight: bold;
-  color: #505458
+  color: #505458;
 }
 .btn button{
   width: 100%;
 }
 .mt20{
   margin-top: 20px;
-  }
+}
 </style>
