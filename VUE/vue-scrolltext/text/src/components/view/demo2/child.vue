2857af7145320dc51e7a0cecb04201bd331d17d8
JZYQAQ <1536729204@qq.com>
2019-06-11 21:45:02 +0800

    props

---
+++ VUE/vue-scrolltext/text/src/components/view/demo2/child.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+      <button @click="sendMsg">ok</button>
+    </div>
+</template>
+
+<script>
+export default {  
+  name: 'demoChild2',
+  data () {
+    return {
+      message: '朝阳'
+    }
+  },
+  methods: {
+    sendMsg () {
+      this.$emit('zy', this.message)
+    }
+  }
+}
+</script>
---
+++ VUE/vue-scrolltext/text/src/components/view/demo2/father.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+      {{msg}}
+      <child @zy="getZy"></child>
+    </div>
+</template>
+
+<script>
+import Child from './child'
+export default {  
+  name: 'demoChild2',
+  data () {
+    return {
+      msg: null
+    }
+  },
+  components: {
+    Child
+  },
+  methods: {
+    getZy (data) {
+      this.msg = data
+    }
+  }
+}
+</script>
