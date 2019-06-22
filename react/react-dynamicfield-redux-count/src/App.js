f131170c678878034caba9278674e1ff1b0989f2
jzy0514 <1536729204@qq.com>
2019-06-22 21:24:47 +0800

    redux

---
+++ react/react-dynamicfield-redux-count/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import 'antd/dist/antd.css';
+import DynamicField from './DynamicField'
+
+function App() {
+  return (
+    <div>
+      <DynamicField />
+    </div>
+  );
+}
+
+export default App;
