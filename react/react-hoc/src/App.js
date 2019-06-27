55a521de85ad6d2e9fadb2d79d215aff883b8f35
jzy0514 <1536729204@qq.com>
2019-06-27 17:38:26 +0800

    react

--- react/react-hoc/src/App.js
+++ react/react-hoc/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 // import logo from './logo.svg';
 import LoginStatus from './LoginStatus';
+import ExampleMobx from './exampleMobx/index';
 import ShopCart from './ShopCart';
 // import WithLogin from './WithLogin';
+import  examplebase  from "./examplebase/index";
+import ExampleDeBounce from './ExampleDeBounce/index'
 import './App.css';
 
 // const WithLoginStatus = WithLogin(LoginStatus); // 包装登录组件
@@ -15,6 +18,8 @@ function App() {
     {/* 经过包装后的组件 */}
     {/* <WithLoginStatus /> */}
     {/* <WithShopCart a="1" b ="2"/> */}
+    <ExampleMobx />
+    <ExampleDeBounce />
     </>
   )
 }
