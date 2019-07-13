03bbfa94682e118cd4cc0a8b432d890d906d734a
jzy0514 <1536729204@qq.com>
2019-07-13 22:09:33 +0800

    1

--- react/wangyiyanxuan/src/pages/home/Home.jsx
+++ react/wangyiyanxuan/src/pages/home/Home.jsx
@@ -1,49 +1,46 @@
 import React, { Component } from 'react';
-import { Layout, Input, Row, Col, Menu, Icon } from 'antd';
+import { Input, Row, Col, Icon } from 'antd';
+import './Home.css';
 import 'antd/dist/antd.css';
 
 const { Search } = Input;
-const { Header, Footer, Content } = Layout;
 class Home extends Component {
   state = {}
   render() {
     return (
       <div>
-        <Layout>
-          <Header className="Top">
-            <Row style={{ lineHeight: '32px' }}>
-              <Col span={6} className="log">
-                <a href="/Home">网易严选</a>
-              </Col>
-              <Col span={14} className="TopSearch">
-                <Search
-                  placeholder="搜索商品 共有xx款好物"
-                  onSearch={value => console.log(value)}
-                  style={{ width: 200 }}  
-                />
-              </Col>
-              <Col span={4}>
-                <div className="loginbtn">登录</div>
-              </Col>
-            </Row>
-            <Menu
-              theme="dark"
-              mode="horizontal"
-              defaultSelectedKeys={['2']}
-              style={{ lineHeight: '32px' }}
-            >
-              <Menu.Item key="1">nav 1</Menu.Item>
-              <Menu.Item key="2">nav 2</Menu.Item>
-              <Menu.Item key="3">nav 3</Menu.Item>
-              <Menu.Item key="4">nav 4</Menu.Item>
-              <div className="more-menu">
-                <Icon type="down" />
-              </div>
-            </Menu>
-          </Header>
-          <Content>Content</Content>
-          <Footer>Footer</Footer>
-        </Layout>
+        <div className="Top">
+          <Row style={{ lineHeight: '32px' }}>
+            <Col span={6} className="log">
+              <a href="/Home" className="logtext">网易严选</a>
+            </Col>
+            <Col span={14} className="TopSearch">
+              <Search
+                placeholder="搜索商品 共有xx款好物"
+                onSearch={value => console.log(value)}
+                style={{ width: 200, backgroundColor: "#ececec" }}
+              />
+            </Col>
+            <Col span={4} style={{ magrenLeft: 0 }}>
+              <div className="loginbtn">登录</div>
+            </Col>
+          </Row>
+          <div className="list">
+            <div className="menu">
+              <span className="text">推荐</span>
+              <span className="text">居家生活</span>
+              <span className="text">服饰鞋包</span>
+              <span className="text">美食酒水</span>
+              <span className="text">个护清洁</span>
+              <span className="text">母婴亲子</span>
+              <span className="text">运动旅游</span>
+              <span className="text">数码家电</span>
+            </div>
+            <div className="more-menu">
+              <Icon type="down" />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
