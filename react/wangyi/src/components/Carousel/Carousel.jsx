88ab200ee1363dc053721644506b647d39a046ad
jzy0514 <1536729204@qq.com>
2019-07-31 14:47:12 +0800

    4

--- react/wangyi/src/components/Carousel/Carousel.jsx
+++ react/wangyi/src/components/Carousel/Carousel.jsx
@@ -6,7 +6,6 @@ class Carousel extends Component {
     super();
     this.state = {
       imgs: [
-        './images/1.png',
         './images/2.png',
         './images/3.png',
         './images/4.png',
--- react/wangyi/src/components/kinds/kind.jsx
+++ react/wangyi/src/components/kinds/kind.jsx
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
 import { Layout, Input, Menu } from 'antd';
+import {
+  BrowserRouter as Router, Route, 
+  Redirect
+} from 'react-router-dom';
+import Recommended from './commodity/recommended/recommended';
+import Life from './commodity/life/life';
+import Bag from './commodity/bag/bag';
+import Drink from './commodity/drink/drink';
+import Clean from './commodity/clean/clean';
+import Mum from './commodity/mum/mum';
+import Motion from './commodity/motion/motion';
+import Digital from './commodity/digital/digital';
 import './kind.css';
 
 const { Header, Sider, Content } = Layout;
@@ -20,61 +32,60 @@ class Kings extends Component {
       });
     }
   };
-  render() { 
+  render() {
     return (
-      <div>
+      <Router>
         <Layout>
-          <Header style={{backgroundColor: "#fff", height: "50px", padding: "0 20px", lineHeight: "50px", }}>
+          <Header style={{ backgroundColor: "#fff", height: "50px", padding: "0 20px", lineHeight: "50px", }}>
             <Search
               placeholder="搜索商品, 共有xx件款好物"
               onSearch={value => console.log(value)}
               style={{ width: 340 }}
             />
           </Header>
-          <Layout style={{borderTop: "1px solid #a1a1a1"}}>
-            <Sider
-            className="sider"
-            width= "88"
-            style={{backgroundColor: "#fff"}}>
-              <ul>
-                <li className="text">
-                  <a href="">推荐专区</a>
-                </li>
-                <li className="text">
-                  <a href="">居家生活</a>
-                </li>
-                <li className="text">
-                  <a href="">居家生活</a>
-                </li>
-                <li className="text">
-                  <a href="">服饰鞋包</a>
-                </li>
-                <li className="text">
-                  <a href="">美食酒水</a>
-                </li>
-                <li className="text">
-                  <a href="">个户清洁</a>
-                </li>
-                <li className="text">
-                  <a href="">母婴亲子</a>
-                </li>
-                <li className="text">
-                  <a href="">运动旅行</a>
-                </li>
-                <li className="text">
-                  <a href="">数码家电</a>
-                </li>
-                <li className="text">
-                  <a href="">全球特色</a>
-                </li>
-              </ul>
+          <Layout style={{ borderTop: "1px solid #a1a1a1" }}>
+            <Sider>
+              <Menu
+                style={{ width: 256, height: '90vh', overflow: 'auto', minWidth: 256 }}
+                defaultOpenKeys={['sub1']}
+                mode="inline"
+              >
+                <Menu.SubMenu key="sub1" title={
+                  <span><Icon type="smile-0"></Icon>部分ui组件实战</span>
+                }>
+                  <Menu.Item key="sub1-1">
+                    {/* /table */}
+                    <Link to='/table'>表格</Link>
+                  </Menu.Item>
+                  <Menu.Item key="sub1-2">
+                    {/* /table */}
+                    <Link to="/label">标签页面</Link>
+                  </Menu.Item>
+                  <Menu.Item key="sub1-3">
+                    {/* /table */}
+                    <Link to="/post/abcdefg">文章详情</Link>
+                  </Menu.Item>
+                </Menu.SubMenu>
+              </Menu>
             </Sider>
-            <Content>Content</Content>
+            <Content>
+                {/* 路由 */}
+                  <Route path="/recommended" component={Recommended} />
+                  <Route path="/life" component={Life} />
+                  <Route path="/bag" component={Bag} />
+                  <Route path="/drink" component={Drink} />
+                  <Route path="/clean" component={Clean} />
+                  <Route path="/mum" component={Mum} />
+                  <Route path="/motion" component={Motion} />
+                  <Route path="/digital" component={Digital} />
+                  {/* <Route path="/" component={Recommend} /> */}
+                  <Redirect from="/" to="/recommended" />
+            </Content>
           </Layout>
         </Layout>
-      </div>
+      </Router>
     );
   }
 }
- 
+
 export default Kings;
