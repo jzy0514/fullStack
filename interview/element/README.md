e536b0c0475adddb0011d1209dfc913c6490ed97
jzy0514 <1536729204@qq.com>
2019-08-10 17:14:17 +0800

    interview

---
+++ interview/element/README.md
@@ -0,0 +1,5 @@
+- 一个框架就好
+- props
+  computed 判断依赖于多个条件时
+  :class="[,{}]"
+  $emit('event, ' )@event="doSonmeEvent"  从内向外传
---
+++ interview/element/src/components/ElButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <button 
+  class="el-button"
+  :disabled="buttonDisabled?'disabled':''"
+  :class="[ 
+  type?'el-button--' + type: '',
+  size?'el-button--' + size: '' ,
+  {
+    'is-disabled': buttonDisabled
+  }
+  ]"
+  @click="handleClick"
+  >
+  <slot></slot> 
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      string: String,
+      default: 'default'
+    },
+    size: {
+      string: String,
+    },
+    disabled: Boolean,
+  },
+  computed: {
+    buttonDisabled () {
+      return this.disabled
+    }
+  },
+  methods: {
+      handleClick (evt) {
+        this.$emit('click', evt);
+      }
+    },
+}
+</script>
+
+<style>
+
+</style>
